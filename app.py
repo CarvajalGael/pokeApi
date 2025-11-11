184c6e100dedbbf0e1d7da5d283548c418c48b77
CarvajalGael <23308060610312@cetis61.edu.mx>
2025-11-11 12:58:57 -0500

    act

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask  import Flask, render_template, request, url_for, flash
+from flask  import Flask, render_template, request,redirect, url_for, flash
 import requests
 
 app = Flask(__name__)
+app.secret_key = 'secreto'
+API = "https://pokeapi.co/api/v2pokemon/"
 
 @app.route("/")
 def inicio():
     return render_template("/index.html")
+
+@app.route("/search",methods=['POST'])
+def search_pokemon():
+    pokemom_name = request.form.get('pokemon_name', '').strip().lower()
+    
+    
+    if not pokemon_mame:
+        flash('Por favor, ingresa un nombre de pokemon.','error')
+        return redirect(url_for('index'))
+
+try:
+    response = requests.get|
+
+if __name__ == '__main__':
+    app.run(debug=True)
