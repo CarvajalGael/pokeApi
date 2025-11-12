a816f8afc52910ce45248d8727e9728382d8ceb1
CarvajalGael <23308060610312@cetis61.edu.mx>
2025-11-12 10:44:43 -0500

    acr

--- app.py
+++ app.py
@@ -14,12 +14,21 @@ def search_pokemon():
     pokemom_name = request.form.get('pokemon_name', '').strip().lower()
     
     
-    if not pokemon_mame:
+    if not pokemon_name:
         flash('Por favor, ingresa un nombre de pokemon.','error')
         return redirect(url_for('index'))
 
 try:
-    response = requests.get|
+    resp = requests.get(f"{API}{pokemon_name}")
+    if resp.status_code == 200:
+        pokemon_data = resp.json()
+        return render_template('pokemon.html', pokemon=pokemon_data)
+    else:
+        flash(f'Pokémon "{pokemon_name}" no encontrado', 'error')
+        return redirect(url_for('index'))
+except requests.exceptions.RequestException as e:
+    flash('Error al buscar el Pokémon', 'error')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
